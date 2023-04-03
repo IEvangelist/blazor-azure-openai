1f15d863d3e00a2817f995a7af6e2e30501c6139
Hussein Beygi <hussein1997.hb@hotmail.com>
2023-04-03 20:06:35 +0330

    Update Azure.OpenAI/Client/Program.cs
    Co-authored-by: David Pine <david.pine@microsoft.com>

--- Azure.OpenAI/Client/Program.cs
+++ Azure.OpenAI/Client/Program.cs
@@ -1,5 +1,3 @@
-using Azure.OpenAI.Client.Extensions;
-
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
